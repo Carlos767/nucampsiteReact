b43ea632e2547b7a72684e1c19a7443c0d001ec7
Carlos Parrilla <cjparrillasf@gmail.com>
2020-04-23 20:31:59 -0700

    components, fixes

--- src/App.js
+++ src/App.js
@@ -2,17 +2,25 @@ import React, { Component } from 'react';
 import Directory from './components/DirectoryComponent';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
+import { CAMPSITES } from './shared/campsites';
 
 class App extends Component {
-    render() {
-        return (
-            <div className="App">
-                <Navbar dark color="primary">
-                <div className="container">
-                    <<NavbarBrand href="/">NuCamp</NavbarBrand>
-                    <Directory />
-                </div>
-                </Navbar>
+  constructor(props) {
+    super(props);
+    this.state = {
+      campsites: CAMPSITES
+    };
+  }
+
+  render() {
+      return (
+           <div className="App">
+              <Navbar dark color="primary">
+              <div className="container">
+                  <<NavbarBrand href="/">NuCamp</NavbarBrand>
+               </div>
+               </Navbar>
+               <Directory campsites={this.state.campsites} />
             </div>
         );
     }
