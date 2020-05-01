e5f8ba2411a4b7fe46efe5b84692d58662802aa1
Carlos Parrilla <cjparrillasf@gmail.com>
2020-04-30 20:58:16 -0700

    React Router

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
-import CampsiteInfo from './CampsiteInfoComponent';
+
 
 class Directory extends Component {
     constructor(props) {
@@ -18,7 +18,7 @@ class Directory extends Component {
         const directory = this.props.campsites.map(campsite => {
             return (
                 <div key={campsite.id} className="col-md-5 m-1">
-                    <Card onClick={() => this.onCampsiteSelect(campsite)}>
+                    <Card>
                         <CardImg width="100%" src={campsite.image} alt={campsite.name} />
                         <CardImgOverlay>
                             <CardTitle>{campsite.name}</CardTitle>
