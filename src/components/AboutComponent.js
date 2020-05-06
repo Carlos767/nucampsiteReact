33fc2e6fc068f7cf32f4255d785e6112c9df5f3a
Carlos Parrilla <cjparrillasf@gmail.com>
2020-05-05 20:21:21 -0700

    routing fixes

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -6,7 +6,7 @@ function RenderPartner({ partner }) {
     if(partner){
         return (
             <React.Fragment>
-                <Media object src={image} alt={name} width="150" />
+                <Media object src={partner.image} alt={partner.name} width="150" />
                 <Media body className="ml-5 mb-4"> 
                     <Media heading>{partner.name}</Media>
                     {partner.description}
@@ -22,7 +22,7 @@ function About(props) {
 
     const partners = props.partners.map(partner => {
         return (
-            <Media tag= "li" key={partners.id}>
+            <Media tag= "li" key={partner.id}>
                 <RenderPartner partner={partner} />
             </Media>
         );
